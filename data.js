02f4f3beda098011b099aa1254e8ade93c0e24cf
suppsialk-cloud <suppsialk@gmail.com>
2025-12-02 14:45:56 +0330

    black rose

---
+++ data.js
@@ -0,0 +1,46 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-blackrosevraydenoiser",
+      "name": "blackrose.VRayDenoiser",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3750,
+      "initialViewParameters": {
+        "yaw": 0.011746390430815268,
+        "pitch": 0.01712825314453248,
+        "fov": 1.3474042771833745
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "blackrose",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
